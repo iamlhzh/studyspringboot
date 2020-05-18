2b011b99a828a039112de64d338cf0f24fd14ea7
ianlhzh <1187563558@qq.com>
2020-05-19 01:59:25 +0800

    初始化

---
+++ src/main/java/cn/lhzh/cn/lhzh/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package cn.lhzh.cn.lhzh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("hello")
+    public String Hello(){
+        return "HelloWorld";
+    }
+}
