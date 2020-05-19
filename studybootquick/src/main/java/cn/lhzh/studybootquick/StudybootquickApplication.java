7dd12e2352b44b4d2000856f69e8ed3263f9377e
ianlhzh <1187563558@qq.com>
2020-05-20 01:21:03 +0800

    增加项目

---
+++ studybootquick/src/main/java/cn/lhzh/studybootquick/StudybootquickApplication.java
@@ -0,0 +1,13 @@
+package cn.lhzh.studybootquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybootquickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybootquickApplication.class, args);
+    }
+
+}
