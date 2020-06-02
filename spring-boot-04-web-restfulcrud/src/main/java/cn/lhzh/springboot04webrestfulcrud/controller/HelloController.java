b1b1f05ac0bc4a1ed9ccc64208798bef98b2a810
ianlhzh <1187563558@qq.com>
2020-06-03 01:42:04 +0800

    1

--- spring-boot-04-web-restfulcrud/src/main/java/cn/lhzh/springboot04webrestfulcrud/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/cn/lhzh/springboot04webrestfulcrud/controller/HelloController.java
@@ -1,16 +1,28 @@
 package cn.lhzh.springboot04webrestfulcrud.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author admin
  * @Date 2020/6/1
  */
-@RestController
+@Controller
 public class HelloController {
+    @ResponseBody
     @RequestMapping("/hello")
-    public String Hello(){
+    public String hello(){
         return "Hello World!";
+    }
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","你好");
+        return "success" ;
     }
 }
