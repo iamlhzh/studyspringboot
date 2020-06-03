8f70aac7a279880d0acbe7020a22f964fd4eda4a
卢宏政 <luhongzheng@changshafubao.com>
2020-06-03 17:35:46 +0800

    1

--- spring-boot-04-web-restfulcrud/src/main/java/cn/lhzh/springboot04webrestfulcrud/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/cn/lhzh/springboot04webrestfulcrud/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +23,8 @@ public class HelloController {
     }
     @RequestMapping("/success")
     public String success(Map<String,Object> map){
-        map.put("hello","你好");
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
         return "success" ;
     }
 }
