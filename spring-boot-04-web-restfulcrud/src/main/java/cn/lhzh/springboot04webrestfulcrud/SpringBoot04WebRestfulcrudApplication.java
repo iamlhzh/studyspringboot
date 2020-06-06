6b27194162e0293fe1040f73be3131bccb5d850c
卢宏政 <luhongzheng@changshafubao.com>
2020-06-06 16:31:03 +0800

    1

--- spring-boot-04-web-restfulcrud/src/main/java/cn/lhzh/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
+++ spring-boot-04-web-restfulcrud/src/main/java/cn/lhzh/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
@@ -2,6 +2,12 @@ package cn.lhzh.springboot04webrestfulcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Converter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBoot04WebRestfulcrudApplication {
@@ -9,5 +15,32 @@ public class SpringBoot04WebRestfulcrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
 	}
+	@Bean
+	public ViewResolver myViewResolver(){
+		return new MyViewResolver();
+	}
+
+
+	private static class MyViewResolver implements ViewResolver{
+
+		@Override
+		public View resolveViewName(String s, Locale locale) throws Exception {
+			return null;
+		}
+	}
+	@Bean
+	public Converter myConverter(){
+
+		return new MyConverter();
+	}
+
+	public static class MyConverter implements Converter{
+
+		@Override
+		public Object convert(Object o, Class aClass, Object o1) {
+			return null;
+		}
+	}
+
 
 }
---
+++ spring-boot-04-web-restfulcrud/src/main/java/cn/lhzh/springboot04webrestfulcrud/config/MyMvcConfig.java
@@ -0,0 +1,27 @@
+package cn.lhzh.springboot04webrestfulcrud.config;
+
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author admin
+ * @Date 2020/6/6
+ */
+@EnableWebMvc
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/success").setViewName("success");
+    }
+}
