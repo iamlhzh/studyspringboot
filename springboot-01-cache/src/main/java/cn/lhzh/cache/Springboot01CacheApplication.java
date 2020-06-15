6c095850eef4507a4441f7a286ce3ed3029935fe
卢宏政 <luhongzheng@changshafubao.com>
2020-06-15 17:30:12 +0800

    1

---
+++ springboot-01-cache/src/main/java/cn/lhzh/cache/Springboot01CacheApplication.java
@@ -0,0 +1,13 @@
+package cn.lhzh.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01CacheApplication.class, args);
+	}
+
+}
---
+++ springboot-01-cache/src/test/java/cn/lhzh/cache/Springboot01CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lhzh.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01CacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
